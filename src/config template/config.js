5c38d8f2490ede76cb65974d589e7eddba7385ed
Johan Coppens <johan.coppens@edugolo.be>
2022-12-19 09:39:38 +0100

    config template

---
+++ src/config template/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  // Vervang <school> zodat url overeenkomt met je school-url
+  apiWSDL: 'https://<school>.smartschool.be/Webservices/V3?wsdl',
+  // Vervang <accessCode> door code geconfigureerd in SmartSchool in
+  // Algemene Configuratie > Webservices
+  // Eventueel kan je daar ook profielen aanmaken
+  accessCode: 'Pa55word',
+  gKeyfileName: 'prefab-manifest-xxxx-xxxx.json',
+  sheetId: '1jJC7kzQ6hXnZg76CkwabXKjqzRyxxxxxxxxxx'
+}
